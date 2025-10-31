86d9822959aba7b4681623c5028592bc29c3ac2f
Gurbaaz <thegillgurbaaz@gmail.com>
2025-10-31 15:37:22 -0600

    Added DeleteTest for failed build

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,5 +12,13 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
 
+        list.deleteCity(edmonton);
+
+        assertFalse(list.hasCity(edmonton));
+    }
 }
