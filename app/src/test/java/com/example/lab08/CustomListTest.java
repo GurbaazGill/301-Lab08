e2e5d4522c8cf69daf05e1250d53e890f8755912
Gurbaaz <thegillgurbaaz@gmail.com>
2025-10-31 15:51:30 -0600

    Added CountCities for failed build

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -21,4 +21,42 @@ public class CustomListTest {
 
         assertFalse(list.hasCity(edmonton));
     }
+
+
+    public void testDeleteCityNotInList() {
+        CustomList list = new CustomList();
+        City vancouver = new City("Vancouver", "BC");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.deleteCity(vancouver);
+        });
+    }
+
+    @Test
+    public void testCountCitiesEmpty() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+    }
+
+    @Test
+    public void testCountCitiesOne() {
+        CustomList list = new CustomList();
+        City toronto = new City("Toronto", "ON");
+        list.addCity(toronto);
+        assertEquals(1, list.countCities());
+    }
+
+    @Test
+    public void testCountCitiesMultiple() {
+        CustomList list = new CustomList();
+        City toronto = new City("Toronto", "ON");
+        City montreal = new City("Montreal", "QC");
+        City vancouver = new City("Vancouver", "BC");
+
+        list.addCity(toronto);
+        list.addCity(montreal);
+        list.addCity(vancouver);
+
+        assertEquals(3, list.countCities());
+    }
 }
